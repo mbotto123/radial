f04159c71dfc95a66edc353f4240bbe76fdcc44c
Marcos Botto Tornielli <mbottot@alum.mit.edu>
2026-06-11 22:53:38 -0400

    Add timing, convergence rates, and iterative solver option

--- examples/uniform_poisson_2D.cc
+++ examples/uniform_poisson_2D.cc
@@ -16,11 +16,15 @@
 #include <deal.II/lac/sparse_matrix.h>
 #include <deal.II/lac/dynamic_sparsity_pattern.h>
 #include <deal.II/lac/sparse_direct.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/precondition.h>
 
 #include <deal.II/numerics/vector_tools.h>
 #include <deal.II/numerics/matrix_tools.h>
 
 #include <deal.II/base/convergence_table.h>
+
+#include <deal.II/base/timer.h>
 
 #include <iostream>
 #include <fstream>
@@ -114,6 +118,8 @@ int main()
   const int min_repetitions = 4;
   // Number of mesh refinements
   const int max_level = 10;
+
+  bool use_iterative_solver = false;
   //-------------------------------------------------------------------------//
 
   for (int order = min_order; order <= max_order; order++)
@@ -121,28 +127,46 @@ int main()
     std::cout << std::endl << "Running P" << order << " case..." << std::endl;
 
     ConvergenceTable convergence_table;
+    std::vector<double> h_vals(max_level);
+    std::vector<double> L2_vals(max_level), L2E_vals(max_level);
+    std::vector<double> H1_vals(max_level), H1E_vals(max_level);
+    std::vector<double> L2_rates(max_level), L2E_rates(max_level);
+    std::vector<double> H1_rates(max_level), H1E_rates(max_level);
 
     for (int level = 0; level < max_level; level++)
     {
       //-------------------------------------------------------------------------//
       // Mesh
+      Timer timer;
+
       int repetitions = min_repetitions * std::pow(std::sqrt(2.0), level);
 
       Triangulation<dim> triangulation;
       GridGenerator::subdivided_hyper_cube_with_simplices(triangulation, repetitions);
+
+      timer.stop();
+      std::cout << std::endl << "Level " << level << " timing:" << std::endl;
+      std::cout << "  Mesh    : " << timer.last_wall_time() << std::endl;
       //-------------------------------------------------------------------------//
 
       //-------------------------------------------------------------------------//
       // Base finite element field
+      timer.start();
+
       const FE_SimplexP<dim> fe(order);
       MappingP1<dim> mapping;
 
       DoFHandler<dim> dof_handler(triangulation); 
       dof_handler.distribute_dofs(fe);
+
+      timer.stop();
+      std::cout << "  FE init : " << timer.last_wall_time() << std::endl;
       //-------------------------------------------------------------------------//
 
       //-------------------------------------------------------------------------//
       // Allocate memory for system matrix, RHS, and solution
+      timer.start();
+
       SparsityPattern sparsity_pattern;
       DynamicSparsityPattern dsp(dof_handler.n_dofs());
 
@@ -152,10 +176,15 @@ int main()
       SparseMatrix<double> system_matrix(sparsity_pattern);
       Vector<double> system_rhs(dof_handler.n_dofs());
       Vector<double> solution(dof_handler.n_dofs());
+
+      timer.stop();
+      std::cout << "  Memory  : " << timer.last_wall_time() << std::endl;
       //-------------------------------------------------------------------------//
 
       //-------------------------------------------------------------------------//
       // Construct Poisson matrix and RHS
+      timer.start();
+
       QGaussSimplex<dim> solution_quadrature(order + 1); // integrate 2p + 1 exactly
 
       MatrixTools::create_laplace_matrix(mapping, dof_handler,
@@ -172,14 +201,34 @@ int main()
                                          system_matrix,
                                          solution,
                                          system_rhs);
+
+      timer.stop();
+      std::cout << "  Assembly: " << timer.last_wall_time() << std::endl;
       //-------------------------------------------------------------------------//
 
       //-------------------------------------------------------------------------//
       // Solve system
-      SparseDirectUMFPACK A_direct;
+      timer.start();
 
-      solution = system_rhs;
-      A_direct.solve(system_matrix, solution);
+      if (use_iterative_solver)
+      {
+        SolverControl            solver_control(1000, 1e-6 * system_rhs.l2_norm());
+        SolverCG<Vector<double>> solver(solver_control);
+        solver.solve(system_matrix, solution, system_rhs, PreconditionIdentity());
+      
+        std::cout << solver_control.last_step()
+                  << " CG iterations needed to obtain convergence." << std::endl;
+      }
+      else
+      {
+        SparseDirectUMFPACK solver;
+
+        solution = system_rhs;
+        solver.solve(system_matrix, solution);
+      }
+
+      timer.stop();
+      std::cout << "  Solve   : " << timer.last_wall_time() << std::endl;
       //-------------------------------------------------------------------------//
 
       //-------------------------------------------------------------------------//
@@ -193,13 +242,20 @@ int main()
 
       //-------------------------------------------------------------------------//
       // Solution recovery
+      timer.start();
+
       Vector<double> solution_enriched(dof_handler_enriched.n_dofs());
       radial::recover_solution_ppr(dof_handler, mapping, solution,
                                    dof_handler_enriched, solution_enriched);
+
+      timer.stop();
+      std::cout << "  Recovery: " << timer.last_wall_time() << std::endl;
       //-------------------------------------------------------------------------//
 
       //-------------------------------------------------------------------------//
       // Compute errors for convergence plots
+      timer.start();
+
       QWitherdenVincentSimplex<dim> error_quadrature(order_enriched + 2);
 
       Vector<double> cell_errors(triangulation.n_cells());
@@ -247,22 +303,47 @@ int main()
       double H1_error_enriched = VectorTools::compute_global_error(triangulation,
                                                                   cell_errors,
                                                                   VectorTools::H1_seminorm);
+      timer.stop();
+      std::cout << "  Error   : " << timer.last_wall_time() << std::endl;
+
+      h_vals[level] = 1 / std::pow(triangulation.n_cells(), 1.0/dim);
+      L2_vals[level] = L2_error;
+      L2E_vals[level] = L2_error_enriched;
+      H1_vals[level] = H1_error;
+      H1E_vals[level] = H1_error_enriched;
+
+      if (level > 0)
+      {
+        double delta_h = std::log(h_vals[level]) - std::log(h_vals[level - 1]);
+        L2_rates[level] = (std::log(L2_vals[level]) - std::log(L2_vals[level - 1])) / delta_h;
+        L2E_rates[level] = (std::log(L2E_vals[level]) - std::log(L2E_vals[level - 1])) / delta_h;
+        H1_rates[level] = (std::log(H1_vals[level]) - std::log(H1_vals[level - 1])) / delta_h;
+        H1E_rates[level] = (std::log(H1E_vals[level]) - std::log(H1E_vals[level - 1])) / delta_h;
+      }
 
       convergence_table.add_value("level", level);
       convergence_table.add_value("cells", triangulation.n_cells());
       convergence_table.add_value("h_E", 1 / std::pow(triangulation.n_cells(), 1.0/dim));
       convergence_table.add_value("L2", L2_error);
+      convergence_table.add_value("L2 rate", L2_rates[level]);
       convergence_table.add_value("L2E", L2_error_enriched);
+      convergence_table.add_value("L2E rate", L2E_rates[level]);
       convergence_table.add_value("H1", H1_error);
+      convergence_table.add_value("H1 rate", H1_rates[level]);
       convergence_table.add_value("H1E", H1_error_enriched);
+      convergence_table.add_value("H1E rate", H1E_rates[level]);
       //-------------------------------------------------------------------------//
     }
 
     convergence_table.set_precision("h_E", 3);
     convergence_table.set_precision("L2", 3);
+    convergence_table.set_precision("L2 rate", 3);
     convergence_table.set_precision("L2E", 3);
+    convergence_table.set_precision("L2E rate", 3);
     convergence_table.set_precision("H1", 3);
+    convergence_table.set_precision("H1 rate", 3);
     convergence_table.set_precision("H1E", 3);
+    convergence_table.set_precision("H1E rate", 3);
     convergence_table.set_scientific("h_E", true);
     convergence_table.set_scientific("L2", true);
     convergence_table.set_scientific("L2E", true);
